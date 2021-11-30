504d555cdfb459dbdea8dc2aa0f92deba171a5f8
Jakub Panienski <jakubpanienski@wp.pl>
2021-11-30 22:19:05 +0100

    reorganisation

---
+++ src/main/java/com/kodilla/firstgamegui/FirstGameGuiApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.firstgamegui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGameGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGameGuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/firstgamegui/FirstGameGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.firstgamegui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGameGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
